9abd8f3260c292c7b06351f0eb9815dfae73b921
Michelle Ng <kalokmng@gmail.com>
2025-10-14 12:24:03 -0400

    Added App Bar in expenses.dart

--- expense_tracker/lib/expenses.dart
+++ expense_tracker/lib/expenses.dart
@@ -26,11 +26,29 @@ class _ExpensesState extends State<Expenses> {
       date: DateTime.now(), 
       category: Category.leisure
     ),
+
+    Expense(
+      title: 'Train Ticket', 
+      amount: 34.00, 
+      date: DateTime.now(), 
+      category: Category.travel,
+    ),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Expense Tracker'),
+          actions:[
+            IconButton(
+              onPressed: () {}, 
+              icon: const Icon(
+                Icons.add
+              ),
+            ),
+          ],
+        ),
       body: 
         Column(
           children: [
