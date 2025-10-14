9f84dc0afe0f5bb1b7fb70fa098562056689ce9f
Michelle Ng <kalokmng@gmail.com>
2025-10-14 12:05:11 -0400

    Finished card styling. Built Expense toolds to forrmat output

--- expense_tracker/lib/expenses_list/expense_item.dart
+++ expense_tracker/lib/expenses_list/expense_item.dart
@@ -24,11 +24,11 @@ class ExpenseItem extends StatelessWidget{
               Text('\$${expense.amount.toStringAsFixed(2)}'),
               const Spacer(),
               Row(children: [
-                  const Icon(Icons.alarm),
+                  Icon(categoryIcons[expense.category]),
                   const SizedBox(
                     width: 8
                     ),
-                  Text(expense.date.toString()),
+                  Text(expense.formattedDate),
                   ],
                 ),
               ],
