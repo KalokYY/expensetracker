e3c7ac5058a94f527ca83dc4f00a09fc5f3dbf9c
Michelle Ng <kalokmng@gmail.com>
2025-10-16 11:50:46 -0400

    Added some scaffolding to the modal. Added textController for reading title.

--- expense_tracker/lib/expenses.dart
+++ expense_tracker/lib/expenses.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/new_expense.dart';
 import 'package:flutter/material.dart';
 
 
@@ -15,7 +16,7 @@ class _ExpensesState extends State<Expenses> {
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
       context: context, 
-      builder: (ctx) => Text('Add Expense'),
+      builder: (ctx) => NewExpense()
     );
   }
 
