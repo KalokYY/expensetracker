2793efa8c2f568dba6d66aa5cbbdac16828a8a29
Michelle Ng <kalokmng@gmail.com>
2025-10-30 12:21:02 -0400

    Finished making widgets for dismissible. Added undo snackbar to retrieve on accidentally delete

--- expense_tracker/lib/expenses.dart
+++ expense_tracker/lib/expenses.dart
@@ -30,6 +30,30 @@ class _ExpensesState extends State<Expenses> {
     );
   }
 
+void _removeExpense(Expense expense)
+  {
+    int expenseIndex = _registeredExpenses.indexOf(expense);
+
+    setState((){
+      _registeredExpenses.remove(expense);
+    });
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Expense Deleted'),
+      duration: Duration(seconds: 3),
+      action: SnackBarAction(
+        label: 'Undo', 
+        onPressed: () {
+          setState(() {
+            _registeredExpenses.insert(expenseIndex, expense);
+              }
+            );
+          },
+        ),
+      ),
+    );
+  }
+
   final List <Expense> _registeredExpenses = [
     Expense(
       title: 'Cheeseburger', 
@@ -55,6 +79,15 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(BuildContext context) {
+    Widget mainContent = const Center(
+      child: Text("Click the + button to add an Expense!"),
+    );
+
+    if(_registeredExpenses.isNotEmpty) {
+      mainContent = ExpensesList(expenses: _registeredExpenses, onRemoveExpense: _removeExpense,
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Expense Tracker'),
@@ -71,7 +104,7 @@ class _ExpensesState extends State<Expenses> {
         Column(
           children: [
             Text("Chart"), 
-            Expanded(child: ExpensesList(expenses: _registeredExpenses)),
+            Expanded(child: mainContent),
         ],
       ),
     );
