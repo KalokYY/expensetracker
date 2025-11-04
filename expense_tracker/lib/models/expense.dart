0a95d8a5fcff9dfab1178823de228c87bc6224c9
Michelle Ng <kalokmng@gmail.com>
2025-11-04 12:28:16 -0500

    Finished Chart

--- expense_tracker/lib/models/expense.dart
+++ expense_tracker/lib/models/expense.dart
@@ -5,9 +5,9 @@ import 'package:uuid/uuid.dart';
 const uuid = Uuid();
 final formatter = DateFormat.yMd();
 
-enum Category {food, travel, leisure, work}
+enum Category { food, travel, leisure, work }
 
-const categoryIcons ={
+const categoryIcons = {
   Category.food: Icons.lunch_dining,
   Category.travel: Icons.flight_takeoff,
   Category.leisure: Icons.movie,
@@ -20,14 +20,34 @@ class Expense {
     required this.amount,
     required this.date,
     required this.category,
-  }): id = uuid.v4();
+  }) : id = uuid.v4();
   final String id;
   final String title;
   final double amount;
-  final DateTime date; 
+  final DateTime date;
   final Category category;
 
-  String get formattedDate{
+  String get formattedDate {
     return formatter.format(date);
+  }
+}
+
+class ExpenseBucket{
+  const ExpenseBucket({required this.category, required this.expenses});
+
+  final Category category;
+  final List<Expense> expenses;
+
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+    : expenses = allExpenses
+    .where((expense)=> expense.category == category)
+    .toList();
+
+  double get totalExpenses{
+    double sum = 0;
+    for(final expense in expenses){
+      sum+=expense.amount;
+    }
+    return sum;
   }
 }
